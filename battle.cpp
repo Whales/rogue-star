7fd5346e8ea4c65e47d17ce99b142de1eb4fb586
Whales <fivedozenwhales@gmail.com>
2013-11-04 15:50:17 -0600

    Adding some battle stuff

---
+++ battle.cpp
@@ -0,0 +1,16 @@
+#include "battle.h"
+#include "cuss.h"
+#include "window.h"
+
+/*
+void Battle::main_loop()
+{
+  cuss::interfact i_battle;
+  if (!i_battle.load_from_file("cuss/i_battle.cuss")) {
+    debugmsg("Couldn't load cuss/i_battle.cuss");
+  }
+
+  Window w_battle(0, 0, 80, 24);
+
+  
+*/
